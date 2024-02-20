a88635367e0a44c3d206599fb0ffa4415429a0f5
Stanislau Baranouski <stan.baranouski@thechefz.co>
2024-02-20 14:08:45 +0300

    fixed SPM support

--- Package.swift
+++ Package.swift
@@ -1,13 +1,17 @@
-//
-//  Package.swift
-//  M13Checkbox
-//
-//  Created by McQuilkin, Brandon on 4/13/16.
-//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
-//
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "M13Checkbox"
+    name: "M13Checkbox",
+    products: [
+        .library(
+            name: "M13Checkbox",
+            targets: ["M13Checkbox"]),
+    ],
+    targets: [
+        .target(
+            name: "M13Checkbox", path: "Sources")
+    ]
 )
